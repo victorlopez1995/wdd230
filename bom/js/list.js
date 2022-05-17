216ca73dde3e698756afa186dd305f7450d7751d
victorlopez1995 <lop22010@byui.edu>
2022-05-16 21:44:03 -0500

    create bom activity

---
+++ bom/js/list.js
@@ -0,0 +1,21 @@
+const input = document.querySelector('input');
+const list = document.querySelector('ul');
+const button = document.querySelector('button');
+
+button.addEventListener('click', function() {
+    const myitem = input.value;
+    if (myitem !== "" ){
+        input.value = '';
+        const myli = document.createElement('li');
+        const mybutton = document.createElement('button');
+        myli.textContent = myitem;
+        mybutton.textContent = '❌';
+        myli.appendChild(mybutton);
+        list.appendChild(myli);
+        mybutton.addEventListener('click', ()=>{
+            return myli.remove();
+        })
+    }
+
+})
+input.focus()
