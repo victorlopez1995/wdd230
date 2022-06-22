6c70018b81ee3ce4e714dea636aab21b169c3f06
victorlopez1995 <lop22010@byui.edu>
2022-06-21 22:14:10 -0500

    use Weather API in chamber of commerce

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,6 +1,45 @@
 const temperature = document.querySelector('#temperature');
 const wind = document.querySelector('#wind');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#condition');
 let chill = document.querySelector('#chill');
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Chia&appid=b844f9ee9f65570ac4f77eb5731d2c13&units=metric';
+
+// weather data------------------------------------------------------------------------------//
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+  function displayResults(weatherData) {
+    temperature.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = capitalize(desc);
+    wind.innerHTML = weatherData.wind.speed;
+  }
+
+  const capitalize = (str) => {
+    let strArr = str.split(" ");
+    return strArr.map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(" ");
+}
+
+
 
 let xtemp = parseFloat(temperature.textContent);
 let y = parseFloat(wind.textContent);
@@ -19,3 +58,6 @@ if ((xtemp <= 50) && (y > 3) ){
     resulttext = "N/A"
     chill.innerHTML = resulttext;
 }
+
+// weather data------------------------------------------------------------------------------//
+
