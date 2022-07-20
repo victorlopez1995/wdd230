a306b51b35cf39e5ca46ed35d798da6c43f3ed75
victorlopez1995 <lop22010@byui.edu>
2022-07-20 18:10:40 -0500

    last updated

--- temple/js/date.js
+++ temple/js/date.js
@@ -2,3 +2,13 @@ const year = document.querySelector('#currentyear');
 const year2 = document.querySelector('#currentyear-2');
 year.textContent = new Date().getFullYear();
 year2.textContent = new Date().getFullYear();
+
+const lastmodific = document.querySelector('#currentdate');
+const lastmodific2 = document.querySelector('#currentdate2');
+
+try {
+    lastmodific.textContent = document.lastModified;
+    lastmodific2.textContent = document.lastModified;
+  } catch (e) {
+    alert('Error with code or your browser does not support Locale');
+  }
